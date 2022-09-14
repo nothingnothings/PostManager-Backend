4c8617dafa3ad6479bf7e6e140e66f665ec7def7
Arthur Panazolo <arthur.panazolo@acad.pucrs.br>
2022-09-14 13:20:29 -0300

    added console.log

--- controllers/feed.js
+++ controllers/feed.js
@@ -48,7 +48,7 @@ exports.getPosts = (req, res, next) => {
 
 exports.createPost = (req, res, next) => {
   const errors = validationResult(req);
-  console.log('ENTERED');
+  console.log('ENTERED', errors);
 
   if (!errors.isEmpty()) {
     const error = new Error('Validation failed, entered data is incorrect.');
